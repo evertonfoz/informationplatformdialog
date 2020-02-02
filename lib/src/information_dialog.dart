b76d9c430681d1eeae743dc479246dec25c5655a
evertonfoz <evertoncoimbra@gmail.com>
2020-02-02 12:38:31 -0300

    1238

--- lib/src/information_dialog.dart
+++ lib/src/information_dialog.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'information_cupertino_alert_dialog.dart';
 import 'information_material_alert_dialog.dart';
 
 class InformationAlertDialog extends StatelessWidget {
@@ -20,7 +21,7 @@ class InformationAlertDialog extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return (!kIsWeb && Platform.isAndroid)
+    return (Platform.isAndroid)
         ? InformationMaterialAlertDialog(
             iconTitle: iconTitle,
             title: title,
@@ -33,51 +34,5 @@ class InformationAlertDialog extends StatelessWidget {
             message: message,
             buttons: buttons,
           );
-  }
-}
-
-class InformationCupertinoAlertDialog extends StatelessWidget {
-  final Icon iconTitle;
-  final String title;
-  final String message;
-  final List<Widget> buttons;
-
-  const InformationCupertinoAlertDialog({
-    this.iconTitle,
-    @required this.title,
-    @required this.message,
-    @required this.buttons,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoAlertDialog(
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          (this.iconTitle != null)
-              ? this.iconTitle
-              : SizedBox(
-                  width: 0,
-                ),
-          (this.iconTitle != null)
-              ? SizedBox(width: 5)
-              : SizedBox(
-                  width: 0,
-                ),
-          Text(this.title),
-        ],
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(
-            height: 25,
-          ),
-          Text(this.message),
-        ],
-      ),
-      actions: buttons,
-    );
   }
 }
