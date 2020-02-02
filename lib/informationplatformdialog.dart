271e7bc60daaaecfc9f26ed7cc5c2cd3632ce020
evertonfoz <evertoncoimbra@gmail.com>
2020-02-02 16:32:42 -0300

    1632

--- lib/informationplatformdialog.dart
+++ lib/informationplatformdialog.dart
@@ -1,7 +1,37 @@
 library informationplatformdialog;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io' show Platform;
+
+import 'package:flutter/material.dart';
+import 'package:informationplatformdialog/src/information_cupertino_alert_dialog.dart';
+import 'package:informationplatformdialog/src/information_material_alert_dialog.dart';
+
+class InformationAlertDialog extends StatelessWidget {
+  final Icon iconTitle;
+  final String title;
+  final String message;
+  final List<Widget> buttons;
+
+  const InformationAlertDialog({
+    this.iconTitle,
+    @required this.title,
+    @required this.message,
+    @required this.buttons,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return (Platform.isAndroid)
+        ? InformationMaterialAlertDialog(
+            iconTitle: iconTitle,
+            title: title,
+            message: message,
+            buttons: buttons,
+          )
+        : InformationCupertinoAlertDialog(
+            iconTitle: iconTitle,
+            title: title,
+            message: message,
+            buttons: buttons,
+          );
+  }
 }
